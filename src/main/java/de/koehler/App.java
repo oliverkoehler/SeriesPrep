d683a8d71706e08aba3eef9b75e3faeae46354ad
olli962 <olli9622@gmail.com>
2020-12-24 23:04:35 +0100

    init

---
+++ src/main/java/de/koehler/App.java
@@ -0,0 +1,26 @@
+package de.koehler;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.*;
+import java.util.Properties;
+
+public class App
+{
+
+    public static void main(String[] args ) throws IOException {
+        Properties prop = new Properties();
+        FileInputStream ip = new FileInputStream(".\\src\\main\\resources\\config.properties");
+        prop.load(ip);
+
+        String path = prop.getProperty("seriesfolder") + prop.getProperty("seriesname") + "\\SHOP\\EN";
+        System.out.println(path);
+        FilesByName test = new FilesByName(path, "webcam");
+        for (File file : test.files){
+            System.out.println(file);
+        }
+
+        File copied = new File("D:\\test.txt");
+        FileUtils.copyFile(test.files[0], copied);
+    }
+}
